f93ae545ada5be3cd49d240c22aca651d9164096
Vojislav <vojislav.stupar@gmail.com>
2019-05-24 19:44:06 +0200

    Zadatak3

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{name}/{age}', function ($name,$age) {
+
+    return view('welcome',compact('name','age'));
 });
 
 Route::get('/about', function () {
